c3b0879e9b94da968618a98b5682a8e2be8490ef
Gabriel Anselmo <gabriel.anselmo.98@gmail.com>
2025-06-26 17:17:07 -0300

    project created

---
+++ src/test/java/com/gabransel/Tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabransel.Tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
